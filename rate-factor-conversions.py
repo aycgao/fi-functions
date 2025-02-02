1d117057935248504a96eec3a4d4a8cb935e1523
alex2021-tech <alx00210@gmail.com>
2025-02-02 17:04:33 -0600

    added rate_factor_converter

--- rate-factor-conversions.py
+++ rate-factor-conversions.py
@@ -4,6 +4,7 @@ Functions:
 - convert_discount_rate_to_discount_factor
 - convert_discount_factor_to_discount_rate
 - spot_forward_factors_conversion
+- rate_factor_converter
 - generate_discount_perfect_system
 """
 
@@ -49,9 +50,54 @@ def spot_forward_factors_conversion(df: pd.DataFrame) -> pd.DataFrame:
     Returns:
         pd.DataFrame: A DataFrame with the same time increments given, but with forward discount factors added
     """
-    df['Forward Discount Factor'] = df['Spot Discount Factor'] / df['Spot Discount Factor'].shift(1)
-    df.iloc[0, df.columns.get_loc('Forward Discount Factor')] = df.iloc[0, df.columns.get_loc('Spot Discount Factor')]
+    df['Forward Discount Factors'] = df['Discount Factors'] / df['Discount Factors'].shift(1)
+    df.iloc[0, df.columns.get_loc('Forward Discount Factors')] = df.iloc[0, df.columns.get_loc('Discount Factors')]
     return df
+
+def rate_factor_converter(input: str, input_df: pd.DataFrame, compounding_freq: int = 2, forward_time_length: int = 0.5) -> pd.DataFrame:
+    ''' 
+    Given any of discount rates, discount factors, forward rates, forward factors, can produce information on the other three in a dataframe
+
+    Params:
+        input (str): string of which input metrics is given (DR: discount rate, DF: discount factor, FR: forward rate, FF: forward factor)
+        input_df (pd.DataFrame): a dataframe with any of the four metrics listed, and the times at which this is occurring (names of columns must be 'Metric', 'Time')
+        compounding_freq (int): the number of compounding periods in a year. "continuous" if continuous compounding. 
+        forward_time_lenght (int): lenght of time for forward rate (T2 - T1)
+    
+    Returns:
+        pd.DataFarme: A dataframe with all four metrics returned
+    '''
+    output = input_df.copy()
+
+    if input == 'DR':
+        output = output.rename(columns = {'Metric': 'Discount Rates'})
+        output['Discount Factors'] = convert_discount_rate_to_discount_factor(output['Discount Rates'], output['Time'], compounding_freq)
+        w = spot_forward_factors_conversion(output)
+        output['Forward Discount Factors'] = w['Forward Discount Factors']
+        output['Forward Discount Rates'] = convert_discount_factor_to_discount_rate(output['Forward Discount Factors'], forward_time_length, compounding_freq)
+        return output
+    
+    if input == 'DF':
+        output = output.rename(columns = {'Metric': 'Discount Factors'})
+        output['Discount Rates'] = convert_discount_factor_to_discount_rate(output['Discount Factors'], output['Time'], compounding_freq)
+        w = spot_forward_factors_conversion(output)
+        output['Forward Discount Factors'] = w['Forward Discount Factors']
+        output['Forward Discount Rates'] = convert_discount_factor_to_discount_rate(output['Forward Discount Factors'], forward_time_length, compounding_freq)
+        return output
+
+    if input == 'FR':
+        output = output.rename(columns = {'Metric': 'Forward Discount Rates'})
+        output['Forward Discount Factors'] = convert_discount_rate_to_discount_factor(output['Forward Discount Rates'], forward_time_length, compounding_freq)
+        output['Discount Factors'] = output['Forward Discount Factors'].cumprod()
+        output['Discount Rates'] = convert_discount_factor_to_discount_rate(output['Discount Factors'], output['Time'], compounding_freq)
+        return output
+
+    if input == 'FF':
+        output = output.rename(columns = {'Metric': 'Forward Discount Factors'})
+        output['Discount Factors'] = output['Forward Discount Factors'].cumprod()
+        output['Forward Discount Rates'] = convert_discount_factor_to_discount_rate(output['Forward Discount Factors'], forward_time_length, compounding_freq)
+        output['Discount Rates'] = convert_discount_factor_to_discount_rate(output['Discount Factors'], output['Time'], compounding_freq)
+        return output
 
 def generate_discount_perfect_system(cash_flow_matrix: pd.DataFrame, prices: pd.DataFrame, freq: int = 2) -> pd.DataFrame:
     """
